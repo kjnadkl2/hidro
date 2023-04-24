77958ee5d32f137dddc6e277653fec461b24bc5a
Alexandre Theis <>
2023-04-24 10:08:58 +0100

    config v0.1

--- deploy-thingsboard.sh
+++ deploy-thingsboard.sh
@@ -2,9 +2,7 @@ wget https://github.com/thingsboard/thingsboard-gateway/releases/latest/download
 
 sudo apt install ./python3-thingsboard-gateway.deb -y
 
-git clone https://github.com/kjnadkl2/hidro.git
-
-sudo mv hidro/tb_gateway.yaml /etc/thingsboard-gateway/config/ && mv hidro/mqtt.json /etc/thingsboard-gateway/config/
+sudo mv tb_gateway.yaml /etc/thingsboard-gateway/config/ && mv mqtt.json /etc/thingsboard-gateway/config/
 
 sudo systemctl restart thingsborad-gateway
 
