a943aece8f74757feb5bf047410a8931e4d758b2
Alexandre Theis <>
2023-04-24 10:13:54 +0100

    change config before apply

--- deploy-thingsboard.sh
+++ deploy-thingsboard.sh
@@ -3,7 +3,3 @@ wget https://github.com/thingsboard/thingsboard-gateway/releases/latest/download
 sudo apt install ./python3-thingsboard-gateway.deb -y
 
 sudo mv tb_gateway.yaml /etc/thingsboard-gateway/config/ && mv mqtt.json /etc/thingsboard-gateway/config/
-
-sudo systemctl restart thingsborad-gateway
-
-systemctl status thingsboard-gateway
